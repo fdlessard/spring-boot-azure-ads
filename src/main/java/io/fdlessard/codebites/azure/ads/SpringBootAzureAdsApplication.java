bffded83016a267e9aa475ba0d1d806b2eac5460
fdlessard <fdlessard@gmail.com>
2020-02-07 20:04:12 -0500

    first commit

---
+++ src/main/java/io/fdlessard/codebites/azure/ads/SpringBootAzureAdsApplication.java
@@ -0,0 +1,13 @@
+package io.fdlessard.codebites.azure.ads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAzureAdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAzureAdsApplication.class, args);
+    }
+
+}
